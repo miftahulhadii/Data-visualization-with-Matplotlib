76b84cdb17abb4bdfd979f5274a3cef61d81f3a5
Miftahul Hadi <78077790+miftahulhadii@users.noreply.github.com>
2022-01-19 19:27:10 +0700

    Update Bar chart.py

--- Bar chart.py
+++ Bar chart.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as hadi #use matplotlib packages for ploting and give your alias "hadi" or whatever
 
+hadi.rcParams["font.family"] = "Book Antiqua" #use matplotlib packages for changing font style of the graph
+hadi.rcParams.update({'font.size': 11.5}) #use matplotlib packages for changing the font size
+
+data = {"Physics":70, "Mathematics":50, "Chemistry":30, "Comp. Science":100} #the data
+
+major = list(data.keys()) #give an instruction to group the major from the data list
+students = list(data.values()) #give an instruction to group the number of students from the data list
+
+#give an instruction to create a bar chart included major and students data
+hadi.bar(major, students, color ="purple") 
+
+hadi.xlabel(r'Major') #give a name for x-axis
+hadi.ylabel('Number of students') #give a name for y-axis
+
+#create a title for the chart
+hadi.title("The number of students in Science Department ITERA")
+
+#create an instruction to show a chart
+hadi.show()
